07e8f72b53d0fbb113a5f564ac63e48b44ab92f6
Henry Paschke <h3nry.pas@gmail.com>
2024-11-04 12:54:57 -0500

    Added setup and readme

---
+++ README.md
@@ -0,0 +1,10 @@
+# GameForge
+
+This library is a collection of useful utility classes and other useful functions for game development in python.
+All these classes were created for specific personal projects, but then I found use for them later in multiple other projects.
+Feel free to open a pull request with any ideas or improvements, or open an issue if there are any problems.
+
+## Installation
+
+```bash
+pip install gameforge
---
+++ setup.py
@@ -0,0 +1,18 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='gameforge',
+    version='1.0',
+    packages=find_packages(),
+    install_requires=[],
+    description='Game development utility classes',
+    author='Henry Paschke',
+    author_email='h3nry.pas@gmail.com',
+    url='https://github.com/henry-paschke/GameForgePython',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+)
